2af035cb23e12a47d3f0407d5e5f074ce1bc5947
joonas.fi <pompo500@xs.fi>
2017-01-25 14:42:46 +0200

    Getting to know Codeship

---
+++ bin/codeship-build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -eu
+
+echo "this is a test build script"
+
+zip -r AtomicCounter.zip src/index.js
---
+++ bin/codeship-deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -eu
+
+echo "okay job complete. AWS_ACCESS_KEY_ID = $AWS_ACCESS_KEY_ID"
