663a927ce78205bb4a2a56ea72adbf7399d5de36
joonas.fi <pompo500@xs.fi>
2017-01-25 18:45:13 +0200

    Publish built .zip file to S3

--- bin/codeship-deploy.sh
+++ bin/codeship-deploy.sh
@@ -2,3 +2,6 @@
 
 echo "# Deploy .zip to Lambda"
 aws lambda update-function-code --function-name AtomicCounter --zip-file fileb://AtomicCounter.zip
+
+echo "# Publishing to https://s3.amazonaws.com/files.function61.com/lambda-atomic-counter/master.zip"
+aws s3 cp AtomicCounter.zip s3://files.function61.com/lambda-atomic-counter/master.zip
